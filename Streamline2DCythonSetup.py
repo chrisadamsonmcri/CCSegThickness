866fc6289607cc566a4aea022697f0c89c058e9d
Chris Adamson <chrisadamson@jonathaysisimac.rch.org.au>
2019-08-12 11:34:45 +1000

    include_dirs for mac os x cython compilation

--- Streamline2DCythonSetup.py
+++ Streamline2DCythonSetup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("Streamline2DCython.pyx")
+    ext_modules = cythonize("Streamline2DCython.pyx"),
+    include_dirs = [numpy.get_include()]
 )
